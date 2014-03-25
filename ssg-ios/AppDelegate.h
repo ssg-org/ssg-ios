9cea7756f0fb531b89e090dbf5a44d343610476f
Haris Dautovic <haris.dautovic@klika.ba>
2014-03-25 10:37:24 +0100

    Init commit

---
+++ ssg-ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ssg-ios
+//
+//  Created by Haris Dautovic on 25/03/14.
+//  Copyright (c) 2014 SSG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ssg-iosTests/ssg_iosTests.m
@@ -0,0 +1,34 @@
+//
+//  ssg_iosTests.m
+//  ssg-iosTests
+//
+//  Created by Haris Dautovic on 25/03/14.
+//  Copyright (c) 2014 SSG. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ssg_iosTests : XCTestCase
+
+@end
+
+@implementation ssg_iosTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
